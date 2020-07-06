242dbff4ea17c7380c838385587d1d09d3e9af44
NIKESH GHIMIRE <nikeshghimire77@gmail.com>
2020-07-05 22:39:25 -0500

    Added the model to the front end

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Listing
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+
+    context = {
+        'listings': listings
+    }
+
+    return render(request, 'listings/listings.html', context)
 
 def listing(request):
     return render(request, 'listings/listing.html')
--- realtors/admin.py
+++ realtors/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Realtor
 
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id','name', 'email')
+    list_display = ('id','name', 'email', 'hire_Date')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
     list_per_page = 25
