5d7c6116fe8521f7da722a8f5918d4456acb384c
NIKESH GHIMIRE <nikeshghimire77@gmail.com>
2020-07-05 23:27:46 -0500

    Added pagination and fixed bugs

--- listings/views.py
+++ listings/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 
 from .models import Listing
+from django.core.paginator import EmptyPage, Paginator
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
 
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
 
     return render(request, 'listings/listings.html', context)
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
