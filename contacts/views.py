00025ed40adc6d89567a016434c1376af50657d0
NIKESH GHIMIRE <nikeshghimire77@gmail.com>
2020-07-08 23:56:54 -0500

    Added funcionalities for making an inquiry to realtor

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,31 @@
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from contacts.models import Contact
+from django.contrib import messages
 
 def contact(request):  
-    return 
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id = listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'Inquiry already made for this property')
+                return redirect('/listings/'+listing_id)
+
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message,
+        user_id=user_id)
+
+        contact.save()
+
+        messages.success(request, 'Your request has been submitted, a realtor will get back to you sson')
+        return redirect('/listings/'+listing_id)
